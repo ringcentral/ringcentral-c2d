1b025958fc9f61f613a1f8f8b220c81f7d2113b8
embbnux <embbnux@embbnux.com>
2019-04-15 15:48:07 +0800

    feat: support to customize ui

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,16 @@ const RC_C2D_MENU_HEIGHT = 30;
 
 const NODE_TEYPE_EXCLUDES = ['STYLE', 'OPTION', 'SCRIPT', 'TEXT', 'TEXTAREA', RC_C2D_ELEM_TAGNAME];
 
+const DEFAULT_LOGO = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="${RC_C2D_MENU_HEIGHT}" height="${RC_C2D_MENU_HEIGHT}" viewBox="0 0 16 16">
+    <g fill="none" fill-rule="evenodd">
+      <rect width="16" height="16" fill="#F80" rx="3.75"/>
+      <path fill="#FFF" d="M5.846 2h4.308c1.337 0 1.822.14 2.311.4.49.262.873.646 1.134 1.135.262.489.401.974.401 2.31v7.976c0 .062-.006.085-.019.108a.127.127 0 0 1-.052.052c-.023.013-.046.019-.108.019H5.846c-1.337 0-1.822-.14-2.311-.4A2.726 2.726 0 0 1 2.4 12.464c-.262-.489-.401-.974-.401-2.31v-4.31c0-1.336.14-1.821.4-2.31A2.726 2.726 0 0 1 3.536 2.4C4.024 2.139 4.509 2 5.845 2z"/>
+      <path fill="#0684BD" d="M5.078 3.813h5.84c.7 0 1.266.566 1.266 1.265v2.953c0 .925-.874 1.505-1.511 1.692.285.54.733 1.356 1.343 2.449H9.953L8.592 9.815h-.088a.28.28 0 0 1-.28-.28V7.883h1.898V5.873H5.881v3.604c0 .6.118 1.64 1.025 2.695H4.843c-.758-.555-1.03-1.689-1.03-2.357V5.078c0-.699.566-1.266 1.265-1.266z"/>
+    </g>
+  </svg>
+`;
+
 function isTelLinkNode(node: HTMLElement) : boolean {
   return node.tagName === 'A' && (node.matches('a[href^="tel:"]') || node.matches('a[href^="sms:"]'));
 }
@@ -67,6 +77,9 @@ export default class ClickToDialInject {
   private _c2dMenuLeft : boolean;
   private _bubblePhoneNumber : boolean;
   private _enabled : boolean;
+  private _logoIcon : string;
+  private _callBtnTitle : string;
+  private _smsBtnTitle : string;
 
   constructor({
     onSmsClick,
@@ -74,12 +87,18 @@ export default class ClickToDialInject {
     countryCode = 'US',
     bubbleInIframe = true,
     enabled = true,
+    logoIcon = DEFAULT_LOGO,
+    callBtnTitle = 'Call with RingCentral',
+    smsBtnTitle = 'SMS with RingCentral',
   } : {
     onSmsClick?: (phoneNumber: number) => void,
     onCallClick?: (phoneNumber: number) => void,
     countryCode: CountryCode,
     bubbleInIframe: boolean,
     enabled: boolean,
+    logoIcon: string,
+    callBtnTitle: string,
+    smsBtnTitle: string,
   }) {
     this._onSmsClickFuncs = [];
     this._onCallClickFuncs = [];
@@ -96,6 +115,9 @@ export default class ClickToDialInject {
     this._c2dMenuEl = null;
     this._c2dNumberHover = false;
     this._currentNumber = null;
+    this._logoIcon = logoIcon;
+    this._callBtnTitle = callBtnTitle;
+    this._smsBtnTitle = smsBtnTitle;
     if (this._enabled) {
       this.start();
     }
@@ -241,21 +263,15 @@ export default class ClickToDialInject {
     this._c2dMenuEl.innerHTML = `
       <div class="rc-widget-c2d-menu-wrapper">
         <div class="rc-widget-c2d-logo">
-          <svg xmlns="http://www.w3.org/2000/svg" width="${RC_C2D_MENU_HEIGHT}" height="${RC_C2D_MENU_HEIGHT}" viewBox="0 0 16 16">
-            <g fill="none" fill-rule="evenodd">
-              <rect width="16" height="16" fill="#F80" rx="3.75"/>
-              <path fill="#FFF" d="M5.846 2h4.308c1.337 0 1.822.14 2.311.4.49.262.873.646 1.134 1.135.262.489.401.974.401 2.31v7.976c0 .062-.006.085-.019.108a.127.127 0 0 1-.052.052c-.023.013-.046.019-.108.019H5.846c-1.337 0-1.822-.14-2.311-.4A2.726 2.726 0 0 1 2.4 12.464c-.262-.489-.401-.974-.401-2.31v-4.31c0-1.336.14-1.821.4-2.31A2.726 2.726 0 0 1 3.536 2.4C4.024 2.139 4.509 2 5.845 2z"/>
-              <path fill="#0684BD" d="M5.078 3.813h5.84c.7 0 1.266.566 1.266 1.265v2.953c0 .925-.874 1.505-1.511 1.692.285.54.733 1.356 1.343 2.449H9.953L8.592 9.815h-.088a.28.28 0 0 1-.28-.28V7.883h1.898V5.873H5.881v3.604c0 .6.118 1.64 1.025 2.695H4.843c-.758-.555-1.03-1.689-1.03-2.357V5.078c0-.699.566-1.266 1.265-1.266z"/>
-            </g>
-          </svg>
+          ${this._logoIcon}
         </div>
-        <div class="rc-widget-action-icon rc-widget-c2d-icon" title="Call with RingCentral">
+        <div class="rc-widget-action-icon rc-widget-c2d-icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 26">
             <path fill="#0083BF" fill-rule="evenodd" stroke="#0684BD" stroke-width=".9" d="M6.656 17.651C.942 10.547.28 4.627 2.545 2.607c.592-.522 2.16-1.323 2.892-1.427.801-.14 1.602.243 2.02 1.01.802 1.636 1.777 3.238 2.788 4.805.452.697.348 1.567-.244 2.16-.349.348-.767.557-1.15.765a3.18 3.18 0 0 0-.801.523s-.105.104-.105.313c-.035.453.035 1.707 2.02 4.214 2.16 2.647 3.45 2.82 3.798 2.82.14 0 .244-.034.244-.034.244-.21.453-.453.662-.697.314-.348.593-.696 1.01-.975.697-.452 1.638-.348 2.16.21 1.325 1.323 2.753 2.576 4.112 3.76.662.557.906 1.428.557 2.16-.244.66-1.359 2.02-2.02 2.541-.558.488-1.324.697-2.195.697-3.345 0-7.7-2.925-11.637-7.8z"/>
           </svg>
         </div>
         <div class="rc-widget-c2d-separator-line"></div>
-        <div class="rc-widget-action-icon rc-widget-c2sms-icon" title="SMS with RingCentral">
+        <div class="rc-widget-action-icon rc-widget-c2sms-icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 22 21">
             <path fill="#0684BD" fill-rule="nonzero" d="M10.944.014C4.91.014.007 4.068.007 9.316c0 2.86 1.54 5.626 4.085 7.417-.094.534-.377 1.257-1.162 2.64l-.692 1.131h1.383a6.976 6.976 0 0 0 4.84-1.917c.817.157 1.634.252 2.483.252 6.034 0 10.936-4.275 10.936-9.523 0-5.28-4.902-9.302-10.936-9.302z"/>
           </svg>
@@ -277,7 +293,13 @@ export default class ClickToDialInject {
 
     this._smsBtn = this._c2dMenuEl.querySelector('.rc-widget-c2sms-icon');
     this._smsBtn.addEventListener('click', () => this._onSmsClick());
+    this.renderBtnTitles();
     document.body.appendChild(this._c2dMenuEl);
+  }
+
+  renderBtnTitles(callBtnTitle = this._callBtnTitle, smsBtnTitle = this._smsBtnTitle) {
+    this._callBtn.setAttribute('title', callBtnTitle);
+    this._smsBtn.setAttribute('title', smsBtnTitle);
   }
 
   _cleanC2DMenu() {
